a08b94a917fe53e6df6644393a8b30c80f450281
Marco Gomez <migomez@ucdavis.edu>
2020-08-18 19:41:41 -0700

    Create Pos struct
    Just an x and a y coordinate :-) May be more trouble that it's worth.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+#include "Pos.hpp"
+
 auto constexpr window_width = 800;
 auto constexpr window_height = 600;
 auto constexpr frame_rate = 60;
@@ -11,19 +13,24 @@ class Ball {
   sf::Vector2f m_velocity;
 
  public:
-  Ball(float x, float y) {
+  Ball(Pos pos) {
     auto constexpr radius = 8.0f;
     auto constexpr speed_x = 10, speed_y = 10;
 
-    m_circle.setPosition(x, y);
+    m_circle.setPosition(pos.x, pos.y);
     m_circle.setRadius(radius);
     m_circle.setFillColor(sf::Color::Red);
     m_circle.setOrigin(radius, radius);
     m_velocity = sf::Vector2f(speed_x, speed_y);
   }
 
-  void Move() { m_circle.move(m_velocity); }
-
+  void Update() { m_circle.move(m_velocity); }
+  float XCoord() { return m_circle.getPosition().x; }
+  float YCoord() { return m_circle.getPosition().y; }
+  float LeftSide() { return XCoord() - m_circle.getRadius(); }
+  float RightSide() { return XCoord() + m_circle.getRadius(); }
+  float TopSide() { return YCoord() - m_circle.getRadius(); }
+  float BottomSide() { return YCoord() + m_circle.getRadius(); }
   sf::CircleShape const& DrawableObject() { return m_circle; }
 };
 
@@ -31,14 +38,13 @@ int main() {
   sf::RenderWindow window({window_width, window_height}, "Portal Brick Break");
   window.setFramerateLimit(frame_rate);
 
-  Ball ball(window_width / 2, window_height / 2);
+  Ball ball({window_width / 2, window_height / 2});
 
   while (!sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
     window.clear(sf::Color::White);
 
-    ball.Move();
+    ball.Update();
     window.draw(ball.DrawableObject());
-
     window.display();
   }
 }
